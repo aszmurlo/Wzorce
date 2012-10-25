49754223ffef13aaf55a51bd38124cfc0db17151
Aleksandra Szmurlo <aleksandra.m.szmurlo@gmail.com>
2012-10-25 13:39:04 +0200

    Master Layout Initial Commit
    Index. Reszta niebawem.

--- WzorceProjektowe/WzorceProjektowe/Controllers/HomeController.cs
+++ WzorceProjektowe/WzorceProjektowe/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WzorceProjektowe.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "msg";
 
             return View();
         }
