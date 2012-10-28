915d578d0f26285449dc1cff765568f8faf9b8e8
Aleksandra Szmurlo <aleksandra.m.szmurlo@gmail.com>
2012-10-28 19:39:01 +0100

    Quiz Layout Initial +Model View Controler Quiz

---
+++ WzorceProjektowe/WzorceProjektowe/Controllers/QuizController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WzorceProjektowe.Models;
+
+namespace WzorceProjektowe.Controllers
+{
+    public class QuizController : Controller
+    {
+        //
+        // GET: /Quiz/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ustawienia Quizu";
+
+            return View(new QuizSettingsModel());
+        }
+
+    }
+}
